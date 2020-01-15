c4ea59a9384a2ddfdef4c99ad0ba8cee768e4f66
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2020-01-15 20:31:04 +0700

    Add What is "this"?

---
+++ 08 - What is "this"?/finished.js
@@ -0,0 +1,68 @@
+(() => {
+  // เริ่มเขียนโค้ด
+
+  // 1. Lexical scope & Dynamic scope
+  function printName() {
+    console.log(this);
+    console.log(`My name is ${this.name}`);
+  }
+
+  // 2. How to know what is "this"?
+  //// 2.1 Invoker object
+  function printName() {
+    console.log(this);
+    console.log(`My name is ${this.name}`);
+  }
+
+  const varayut = { name: 'Varayut', printName };;
+  const jane = { name: 'Jane', printName };
+
+  varayut.printName();
+  jane.printName();
+
+  //// 2.2 Global object (window, global)
+  function printName() {
+    console.log(this);
+    console.log(`My name is ${this.name}`);
+  }
+
+  name = 'Global';
+  printName();
+
+  //// 2.3 Constructor function
+  function printName() {
+    console.log(this);
+    console.log(`My name is ${this.name}`);
+  }
+  function Person (name) {
+    this.name = name
+
+    printName();
+  }
+
+  const varayut = new Person('Varayut');
+
+
+  // 3. call(), apply(), and bind()
+  function printName(nationality, city) {
+    console.log(this);
+    console.log(
+      `My name is ${this.name}, I'm ${nationality} and am living in ${city}`
+    );
+  }
+
+  function Person (name, nationality, city) {
+    this.name = name
+    this.nationality = nationality
+    this.city = city;
+
+    printName(this.nationality, this.city);
+    printName.call(this, 'Thai', 'Bangkok');
+    printName.apply(this, ['Thai', 'Bangkok']);
+
+    const printVarayutName = printName.bind(this);
+    printVarayutName('Thai', 'Bangkok');
+  }
+
+  const varayut = new Person('Varayut', 'Thai', 'City');
+})();
---
+++ 08 - What is "this"?/start.js
@@ -0,0 +1,10 @@
+(() => {
+  // เริ่มเขียนโค้ด
+
+  // 1. Lexical scope & Dynamic scope
+  // 2. How to know what is "this"?
+  //// 2.1 Invoker object
+  //// 2.2 Global object (window, global)
+  //// 2.3 Constructor function
+  // 3. call(), apply(), and bind()
+})();
