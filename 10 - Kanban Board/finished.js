73ad9997280db8f000e7002f59f91334ea946f6a
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2020-01-16 21:59:32 +0700

    Add Kanban Board

---
+++ 10 - Kanban Board/finished.js
@@ -0,0 +1,36 @@
+(() => {
+  let draggingElem;
+
+  function onDragStart() {
+    draggingElem = this;
+  }
+  
+  function onDrop() {
+    this.append(draggingElem);
+    draggingElem = null;
+  }
+
+  function onDragOver() {
+    event.preventDefault();
+  }
+  
+  function onDragEnter() {
+    event.preventDefault();
+  }
+  
+  function run() {
+    const taskElems = Array.from(document.querySelectorAll('.task'));
+    const dropZoneElems = Array.from(document.querySelectorAll('.drop-zone'));
+    
+    taskElems.forEach((taskElem) => {
+      taskElem.addEventListener('dragstart', onDragStart);
+    });
+
+    dropZoneElems.forEach((dropZoneElem) => {
+      dropZoneElem.addEventListener('drop', onDrop);
+      dropZoneElem.addEventListener('dragover', onDragOver);
+      dropZoneElem.addEventListener('dragenter', onDragEnter);
+    });
+  }
+  run();
+})();
