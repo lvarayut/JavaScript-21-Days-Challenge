ea0ffdd99639a36e9785bd1085d2a5b1c2c8d25b
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2020-02-13 20:35:58 +0700

    Fix wrong a variable name in Kanban Board project

--- 09 - Parallax Scrolling/finished.js
+++ 09 - Parallax Scrolling/finished.js
@@ -1,9 +1,9 @@
 (() => {
   function onScroll() {
-    const sunElem = document.querySelector('.moon');
+    const moonElem = document.querySelector('.moon');
     const wishElem = document.querySelector('.wish');
 
-    sunElem.style.transform = `translate(${window.scrollY * 0.7}%, ${window.scrollY * -0.7}%)`;
+    moonElem.style.transform = `translate(${window.scrollY * 0.7}%, ${window.scrollY * -0.7}%)`;
     wishElem.style.transform = `translateY(${window.scrollY * -1.2}%)`;
   }
 
