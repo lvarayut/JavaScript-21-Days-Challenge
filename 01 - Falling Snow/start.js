d0cd122e92616eb2856b25a2c0e7910f74ea00e5
bearmilkfrappe <primkind@gmail.com>
2024-10-16 10:25:21 +0900

    เสร็จหิมะตกเย้ๆ

--- 01 - Falling Snow/start.js
+++ 01 - Falling Snow/start.js
@@ -1,4 +1,72 @@
 (() => {
   // เริ่มเขียนโค้ด
+  function setup() {
+    const canvas = document.getElementById('falling-snow-canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
+    return {
+      canvas,
+      canvasContext: canvas.getContext('2d'),
+      numberOfSnowballs: 250
+
+    };
+  }
+
+  function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function createSnowballs(canvas, numberOfSnowballs) {
+    return [...Array(numberOfSnowballs)].map(() => {
+      return {
+        x: random(0, canvas.width),
+        y: random(0, canvas.height),
+        opacity: random(0.5,1),
+        radius: random(2,4),
+        speedX: random(-5,5),
+        speedY: random(1,3)
+      }
+    });
+
+  }
+
+  function drawSnowBall(canvasContext, snowBall) {
+    canvasContext.beginPath();
+    canvasContext.arc(snowBall.x, snowBall.y, snowBall.radius, 0, Math.PI * 2);
+    canvasContext.fillStyle = `rgba(255, 255, 255, ${snowBall.opacity})`;
+    canvasContext.fill();
+  }
+  function moveSnowBall(canvas, snowBall) {
+    snowBall.x += snowBall.speedX;
+    snowBall.y += snowBall.speedY;
+
+    if(snowBall.x > canvas.width) {
+      snowBall.x = 0;
+    } else if (snowBall.x < 0) {
+      snowBall.X = canvas.width;
+    }
+
+    if (snowBall.y > canvas.height) {
+      snowBall.y = 0;
+    }
+  }
+
+  function run() {
+    const { canvas, canvasContext, numberOfSnowballs } = setup();
+    const snowBalls = createSnowballs(canvas, numberOfSnowballs);
+
+    setInterval(() => {
+      canvasContext.clearRect(0,0,canvas.width,canvas.height);
+      snowBalls.forEach((snowBall) => drawSnowBall(canvasContext, snowBall));
+      snowBalls.forEach((snowBall) => moveSnowBall(canvas, snowBall));
+    }, 50)
+    
+
+  }
+  
+
+ 
+
+ run();
 })();
